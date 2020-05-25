4cdb87d069ee86779806675535d44bc2e7d96702
Darren Sarra <adriansarra18@gmail.com>
2020-05-24 21:21:11 -0400

    Created a link to html-routes.js file on the server.js file.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-//app.use(require("./"))
+app.use(require("./routes/html-routes"));
 
 app.listen(PORT, ()=> {
     console.log(`App running on port ${PORT}`);
