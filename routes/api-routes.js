18385a595259a42c0b6641eb8ae6b0e81c53a1fd
Darren Sarra <adriansarra18@gmail.com>
2020-05-24 23:27:36 -0400

    Fixed typo.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,8 +3,8 @@ const Workout = require("../models/workout");
 
 router.post("/api/workouts", ({body}, res) => {
     Workout.create(body)
-        .then(workout => {
-            res.json(workout);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
@@ -13,11 +13,23 @@ router.post("/api/workouts", ({body}, res) => {
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-        .then(workout => {
-            res.json(workout);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({body,params}, res) => {
+    Workout.findByIdAndUpdate(params._id,
+        {
+            $push: {
+                exercises: body
+            }
+        }, (err, data) => {
+            if (err) throw err;
+            res.send(data);
         });
 });
 
